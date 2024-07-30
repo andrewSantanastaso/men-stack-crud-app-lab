d29f67be9d3f03475d87f50474c979eb91249573
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-07-30 09:46:20 -0400

    set up complete

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const MONGO_URI = process.env.MONGO_URI
+const PORT = 3000
+
+
+mongoose.connect(MONGO_URI)
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB is connected and running')
+})
+mongoose.connection.on('error', () => {
+    console.log("MongoDB connection error")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Express running on port:${PORT}`)
+})
